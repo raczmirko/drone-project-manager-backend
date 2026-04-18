e92424a958554dbdbe1371058a6de6fc52d6cacf
raczmirko <git@okrim.hu>
2026-04-18 12:06:32 +0200

    feat: initial project setup

---
+++ src/main/java/hu/okrim/droneprojectmanager/DroneProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.okrim.droneprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/okrim/droneprojectmanager/DroneProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.okrim.droneprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
