cdd48efbd96fade68b316fb4dfe6c48b0aa1925d
raczmirko <git@okrim.hu>
2026-04-19 18:38:29 +0200

    refactor: optimized imports

--- src/main/java/hu/okrim/droneprojectmanager/security/filter/AuthenticationFilter.java
+++ src/main/java/hu/okrim/droneprojectmanager/security/filter/AuthenticationFilter.java
@@ -4,23 +4,19 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.okrim.droneprojectmanager.dto.UserDto;
-import hu.okrim.droneprojectmanager.model.User;
 import hu.okrim.droneprojectmanager.security.SecurityConstants;
 import hu.okrim.droneprojectmanager.security.manager.CustomAuthenticationManager;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
 import java.util.Date;
-import java.util.List;
 
 @AllArgsConstructor
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
