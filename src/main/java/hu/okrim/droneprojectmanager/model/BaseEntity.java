8970d946fc7f378b4ac5f2800710c0044e6dd09d
raczmirko <git@okrim.hu>
2026-04-21 08:22:59 +0200

    feat: enabled JPA auditing

---
+++ src/main/java/hu/okrim/droneprojectmanager/model/BaseEntity.java
@@ -0,0 +1,35 @@
+package hu.okrim.droneprojectmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+@RequiredArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    private UUID id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
