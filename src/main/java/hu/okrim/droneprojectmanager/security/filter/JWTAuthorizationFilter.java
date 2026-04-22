9dac47a0686c529638e215963d2c9491d276bce7
raczmirko <git@okrim.hu>
2026-04-22 11:28:00 +0200

    fix: added default role to JWT token otherwise all requests result in a 403

--- src/main/java/hu/okrim/droneprojectmanager/security/filter/JWTAuthorizationFilter.java
+++ src/main/java/hu/okrim/droneprojectmanager/security/filter/JWTAuthorizationFilter.java
@@ -9,10 +9,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
@@ -31,7 +33,9 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
                 .verify(token)
                 .getSubject();
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null);
+        // This default role is required, otherwise Spring Security will throw an exception
+        List<GrantedAuthority> authorities = List.of(() -> "ROLE_USER");
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
