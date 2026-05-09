a1758f0810ff703af0024acb0a251c0a8bec3312
raczmirko <git@okrim.hu>
2026-05-09 14:45:06 +0200

    refactor: optimized imports

--- src/main/java/hu/okrim/droneprojectmanager/model/ProjectFile.java
+++ src/main/java/hu/okrim/droneprojectmanager/model/ProjectFile.java
@@ -1,13 +1,15 @@
 package hu.okrim.droneprojectmanager.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.util.UUID;
 
 @Getter
 @Setter
